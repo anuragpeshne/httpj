6a2a7096efa7f265737e1b0d0ea7266b82abf2f9
anuragpeshne <anurag.peshne@gmail.com>
2016-11-08 22:10:22 -0500

    request parser completed, hello HTTP/1.0

--- src/httpj/core.clj
+++ src/httpj/core.clj
@@ -1,5 +1,7 @@
 (ns httpj.core
   (:gen-class)
+  (:require [clojure.string :as str]
+            [clojure.pprint])
   (:import [java.net.ServerSocket]
            [java.net.Socket]
            [java.io.BufferedReader]
@@ -7,23 +9,57 @@
 
 (def PORT 8080)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!")
-  (let [listenerSocket (new java.net.ServerSocket PORT)]
-  (while true
-    (let [clientSocket (.accept listenerSocket)
-          in (new java.io.BufferedReader
-                  (new java.io.InputStreamReader (.getInputStream clientSocket)))
-          out (new java.io.PrintWriter (.getOutputStream clientSocket))
-          clientHandler (future
-                          (println "got a connection@" (.getRemoteSocketAddress clientSocket))
-                          (doseq [line (line-seq in)]
-                            (.println out line)
-                            (.flush out)))]))))
+(defrecord ReqHeader [method path version])
 
-(defn clientHandler
+(defn parseHeadLine
+  [line]
+  (let [tokens (str/split line #" ")
+        method (cond (= (str/upper-case (get tokens 0)) "GET") :GET
+                     (= (str/upper-case (get tokens 0)) "POST") :POST
+                     :else "unknown") ;;probably throw exception
+        path (get tokens 1)
+        version (get tokens 2)]
+    (ReqHeader. method path version)))
+
+(defn generateOutput
+  [parsedReq]
+  (println "in generate output")
+  (str "HTTP/1.0 200 OK\r\n"
+       "Server: httpj/x.x\r\n"
+       "\r\n"
+       "Hello World!\r\n"))
+
+(defn parseReqest
+  "parses and returns request obj"
+  [in]
+  (let [inp (line-seq in)
+        headLine (parseHeadLine (first inp))
+        headers (loop [curInp (rest inp) list []]
+                  (if (or (= (first curInp) "") (nil? (first curInp))) list
+                      (recur (rest curInp)
+                             (conj list (apply hash-map
+                                               (str/split (first curInp) #": "))))))]
+    (println "printing headers")
+    (clojure.pprint/pprint headers)
+    {:headLine headLine, :headers headers}))
+
+(defn handleClient
   "This function is executed after accepting socket"
   [socket]
-  (println "hello"))
+  (let [in (new java.io.BufferedReader
+                (new java.io.InputStreamReader (.getInputStream socket)))
+        out (new java.io.PrintWriter (.getOutputStream socket))
+        clientHandler (future
+                        (println "got a connection@" (.getRemoteSocketAddress socket))
+                        (.print out (generateOutput (parseReqest in)))
+                        (.flush out)
+                        (.close socket))]))
+
+(defn -main
+  "Starting point for httpj"
+  [& args]
+  (println "Server ready.")
+  (let [listenerSocket (new java.net.ServerSocket PORT)]
+    (while true
+      (let [clientSocket (.accept listenerSocket)]
+        (handleClient clientSocket)))))
