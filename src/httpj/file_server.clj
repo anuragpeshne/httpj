94e376d385fdc144db3e9cc685bfcfbb1a5b5b3f
anuragpeshne <anurag.peshne@gmail.com>
2016-11-14 19:20:16 -0500

    dirty changes for file cache; towards open-file-table

--- src/httpj/file_server.clj
+++ src/httpj/file_server.clj
@@ -1,9 +1,19 @@
 (ns httpj.file-server
   (:gen-class)
+  (:require [clojure.core.cache :as cache]
+            [clojure.java.io/file]
+            [clojure.java.io/reader])
   (:import [java.io.FileNotFoundException]))
+
+(def file-cache (cache/lu-cache-factory))
 
 (defn get-file
   [file-path]
-  (try
-    (slurp file-path)
-    (catch java.io.FileNotFoundException e nil)))
+  (if (cache/has? file-cache file-path)
+    (let [f (cache/hit file-cache file-path)
+          rdr (clojure.java.io/reader f)]
+      rdr)
+    (let [f (clojure.java.io/file file-path)]
+      (if (and (.exists f) (not (.isDirectory f)))
+        (cache/miss file-cache file-path f)
+        nil))))
